1a8f9948e971f639eb8dc43b3da2dc8cc6937f7f
nam2626 <nam2626@naver.com>
2023-06-30 11:42:45 +0900

    JSP 기초

--- Step01/src/main/java/servlet/LoginServlet.java
+++ Step01/src/main/java/servlet/LoginServlet.java
@@ -26,8 +26,11 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//사용자가 폼을 이용해서 보낸 값을 읽어오는 부분
+		//getParameter로 쿼리스트링으로 보낸 값을 읽음, input 태그의 name 속성 값으로 보낸 값을 읽어옴
+		String id = request.getParameter("id");
+		String passwd = request.getParameter("passwd");
+		System.out.println(id + " , " + passwd);
 	}
 
 	/**
