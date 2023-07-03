baed8bf8bd3cdc4fd3c26d5ca4d27c5c9538858f
nam2626 <nam2626@naver.com>
2023-07-03 16:49:13 +0900

    로그인

--- MemberMangeProject/src/main/java/dao/MemberDAO.java
+++ MemberMangeProject/src/main/java/dao/MemberDAO.java
@@ -1,5 +1,9 @@
 package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import config.DBManager;
 import dto.MemberDTO;
 
@@ -19,7 +23,26 @@ public class MemberDAO {
 
 	public MemberDTO login(String id, String passwd) {
 		MemberDTO dto = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 		//select 문으로 회원정보 조회 후 그 결과로 MemberDTO 객체로 만들어서 리턴
+		String sql = "select * from member where member_id like ? and member_passwd like ?";
+		
+		try {
+			pstmt = manager.getConn().prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.setString(2, passwd);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				dto = new MemberDTO(rs.getString(1), rs.getString(2), rs.getString(3), 
+						rs.getInt(4), rs.getString(5).charAt(0));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			manager.close(rs, pstmt);
+		}
 		
 		return dto;
 	}
