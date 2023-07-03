0cf52d393f26c9b09d959e80bbd1b1505615cc37
nam2626 <nam2626@naver.com>
2023-07-03 09:48:55 +0900

    쿠키

---
+++ Step02/src/main/java/page/CookieServlet.java
@@ -0,0 +1,58 @@
+package page;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CookieServlet
+ */
+@WebServlet("/cookie_setting")
+public class CookieServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CookieServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String key = request.getParameter("key");
+		String val = request.getParameter("val");
+		
+		//쿠키 생성
+		Cookie cookie = new Cookie(key, val);
+		cookie.setMaxAge(60*60*24);
+		cookie.setPath("/");
+		
+		response.addCookie(cookie);//쿠키를 등록
+		response.sendRedirect("cookie_result.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+
+
+
+
+
+
+
+
---
+++ Step02/src/main/webapp/07_cookie_start.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="cookie_setting">
+		<input type="text" name="key" placeholder="키값"><br>
+		<input type="text" name="val" placeholder="쿠키값"><br>
+		<button>전송</button>
+	</form>
+</body>
+</html>
