d4a249ab0c8896cee3716bdff5f20b0a9acbf0bd
nam2626 <nam2626@naver.com>
2023-07-06 09:22:28 +0900

    마이바티스 예제

--- MybatisExample/src/dao/MemberMapper.java
+++ MybatisExample/src/dao/MemberMapper.java
@@ -1,5 +1,19 @@
 package dao;
 
-public class MemberMapper {
+import config.DBManager;
 
+public class MemberMapper {
+	private static MemberMapper instance = new  MemberMapper();
+	private DBManager manager;
+	private MemberMapper() {
+		manager = DBManager.getInstance();
+	}
+
+	public static MemberMapper getInstance() {
+		if(instance == null)
+			instance = new MemberMapper();
+		return instance;
+	}
+	
+	
 }
