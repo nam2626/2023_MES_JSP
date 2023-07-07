ed6b4160c3cb41a761ac039e1fcf51bfff3d358a
nam2626 <nam2626@naver.com>
2023-07-07 11:07:42 +0900

    게시판

--- BoardProject/src/main/java/controller/HandlerMapping.java
+++ BoardProject/src/main/java/controller/HandlerMapping.java
@@ -2,5 +2,33 @@ package controller;
 //싱글톤
 //createController(command)
 public class HandlerMapping {
+	private static HandlerMapping instance = new HandlerMapping();
 
+	private HandlerMapping() {	}
+
+	public static HandlerMapping getInstance() {
+		if(instance == null)
+			instance = new HandlerMapping();
+		return instance;
+	}
+	
+	public Controller createController(String command) {
+		Controller controller = null;
+		
+		switch(command) {
+		
+		}
+		
+		return controller;
+	}
+	
+	
+	
+	
 }
+
+
+
+
+
+
