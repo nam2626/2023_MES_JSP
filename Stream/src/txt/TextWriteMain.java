9fb5ef3b95fac6c5f4f97103ff646dcf0c546526
nam2626 <nam2626@naver.com>
2023-07-10 17:53:55 +0900

    파일 입출력

--- Stream/src/txt/TextWriteMain.java
+++ Stream/src/txt/TextWriteMain.java
@@ -1,9 +1,38 @@
 package txt;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class TextWriteMain {
 
 	public static void main(String[] args) {
-
+		FileWriter fw = null;
+		PrintWriter pw = null;
+		
+		try {
+			//true => 이어서 쓰기, false => 새파일 생성
+			fw = new FileWriter("out.txt",true);
+			pw = new PrintWriter(fw);
+			
+			pw.println("Hello World");
+			pw.flush();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			if(pw!=null)pw.close();
+		}
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
