b6a085548fc09cf769af15ae245abdd0e87334f4
nam2626 <nam2626@naver.com>
2023-07-11 12:29:21 +0900

    파일 입출력

--- Stream/src/file/FileCopy.java
+++ Stream/src/file/FileCopy.java
@@ -14,12 +14,12 @@ public class FileCopy {
 		//text.txt를 c:\\2023_07_11\\copy.txt 로 복사
 		//FileInputStream, FileOutputStream, Calendar, SimpleDateFormat, File
 		//원본파일 선택
-		File originFile = new File("text.txt");
+		File originFile = new File("stream.pdf");
 		Calendar cal = Calendar.getInstance();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy_MM_dd");
 		System.out.println(sdf.format(cal.getTime()));
 		//복사할 위치 지정
-		File copyFile = new File("c:\\"+sdf.format(cal.getTime())+"\\copy.txt");
+		File copyFile = new File("c:\\"+sdf.format(cal.getTime())+"\\copy.pdf");
 		//복사할 경로까지의 폴더가 존재하는지 확인 후 없으면 폴더 생성
 		if(!copyFile.getParentFile().exists())
 			copyFile.getParentFile().mkdirs();
@@ -27,13 +27,22 @@ public class FileCopy {
 		try(FileInputStream fis = new FileInputStream(originFile);
 			FileOutputStream fos = new FileOutputStream(copyFile)){
 			
-			int data = 0;
-			
-			while((data = fis.read()) != -1) {
-				fos.write(data);
+//			int data = 0;
+			long startTime = System.currentTimeMillis();
+//			while((data = fis.read()) != -1) {
+//				fos.write(data);
+//				fos.flush();
+//			}
+			byte[] buffer = new byte[100];
+			while(true) {
+				int count = fis.read(buffer);
+				if(count == -1) break;
+				fos.write(buffer, 0, count);
 				fos.flush();
 			}
 			
+			long endTime = System.currentTimeMillis();
+			System.out.println(endTime - startTime);
 			System.out.println("파일 복사 완료");
 			
 		} catch (FileNotFoundException e) {
