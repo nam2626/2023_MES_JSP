11075128aeadd242c788dee5d63ec1336b746d6f
nam2626 <nam2626@naver.com>
2023-07-11 09:49:24 +0900

    파일 입출력

--- Stream/src/data/DataReadTest.java
+++ Stream/src/data/DataReadTest.java
@@ -1,9 +1,52 @@
 package data;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class DataReadTest {
 
 	public static void main(String[] args) {
+		FileInputStream fis = null;
+		DataInputStream dis = null;
+		
+		try {
+			fis = new FileInputStream("data.dat");
+			dis = new DataInputStream(fis);
 
+			int n = dis.readInt();
+			double pi = dis.readDouble();
+			char ch = dis.readChar();
+			boolean flag = dis.readBoolean();
+			
+			System.out.println(n);
+			System.out.println(pi);
+			System.out.println(ch);
+			System.out.println(flag);
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(dis != null)dis.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 }
+
+
+
+
+
+
+
+
+
