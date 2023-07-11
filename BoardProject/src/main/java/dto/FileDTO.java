492e34fe85d7737a59d8fe4f635526579c1af77e
nam2626 <nam2626@naver.com>
2023-07-11 12:43:47 +0900

    파일 업로드

--- BoardProject/src/main/java/dto/FileDTO.java
+++ BoardProject/src/main/java/dto/FileDTO.java
@@ -9,31 +9,87 @@ public class FileDTO {
 	private int bno;
 	private int fno;
 
-	public FileDTO() {	}
+	public FileDTO() {
+	}
 
 	public FileDTO(File file, int bno, int fno) {
 		this.bno = bno;
 		this.fno = fno;
 		this.path = file.getAbsolutePath();
 		this.fileName = file.getName();
-		switch(fileName.substring(fileName.lastIndexOf('.')+1)) {
+		switch (fileName.substring(fileName.lastIndexOf('.') + 1)) {
 		case "png":
 		case "jpg":
 		case "gif":
 		case "bmp":
 			this.type = "image";
-			 break;
+			break;
+		case "mp4":
+			this.type = "video";
+			break;
+		default:
+			this.type = "normal";
 		}
 	}
-	
-	
-	
-	
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+		File file = new File(path);
+		switch (file.getName().substring(file.getName().lastIndexOf('.') + 1)) {
+		case "png":
+		case "jpg":
+		case "gif":
+		case "bmp":
+			this.type = "image";
+			break;
+		case "mp4":
+			this.type = "video";
+			break;
+		default:
+			this.type = "normal";
+		}
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getBno() {
+		return bno;
+	}
+
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+
+	public int getFno() {
+		return fno;
+	}
+
+	public void setFno(int fno) {
+		this.fno = fno;
+	}
+
+	@Override
+	public String toString() {
+		return "FileDTO [path=" + path + ", fileName=" + fileName + ", type=" + type + ", bno=" + bno + ", fno=" + fno
+				+ "]";
+	}
+
 }
-
-
-
-
-
-
-
