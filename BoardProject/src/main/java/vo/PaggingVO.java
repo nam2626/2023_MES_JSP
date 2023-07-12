b780ca902745849f879d596af36058d3720029d3
nam2626 <nam2626@naver.com>
2023-07-12 10:34:00 +0900

    페이징

--- BoardProject/src/main/java/vo/PaggingVO.java
+++ BoardProject/src/main/java/vo/PaggingVO.java
@@ -23,14 +23,30 @@ public class PaggingVO {
 
 	//전체 페이지 개수 : 전체 게시글 개수 / 한페이지당 출력할 게시글 개수 + (나머지가 0 아니면 1)
 	public int getTotalPage() {
-		return count / pageOfContentCount + (count % pageGroupOfCount == 0 ? 0 : 1);
+		return count / pageOfContentCount + (count % pageOfContentCount == 0 ? 0 : 1);
 	}
 	
+	//전체 페이지 그룹 개수 : 전체 페이지 개수 / 게시판 하단에 나타낼 페이지 번호 개수 + (나머지가 0 아니면 1)
+	public int getTotalPageGroup() {
+		return getTotalPage() / pageGroupOfCount + (getTotalPage() / pageGroupOfCount == 0 ? 0 : 1);
+	}
 	
-	
-	
+	//현재 페이지 그룹 번호
+	public int getNowPageGroupNo() {
+		
+	}
 	
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
