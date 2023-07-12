358dd8ff3eacce40c94cda468bd0e0f8134b49a8
nam2626 <nam2626@naver.com>
2023-07-12 11:09:20 +0900

    페이징

--- BoardProject/src/main/java/vo/PaggingVO.java
+++ BoardProject/src/main/java/vo/PaggingVO.java
@@ -33,8 +33,38 @@ public class PaggingVO {
 	
 	//현재 페이지 그룹 번호
 	public int getNowPageGroupNo() {
-		
+		return currentPageNo / pageGroupOfCount + (currentPageNo / pageGroupOfCount == 0 ? 0 : 1);
 	}
+	
+	//현재 페이지 그룹 시작 페이지 번호 : (현재 페이지 그룹 번호 - 1) * 게시판 하단에 나타낼 페이지 번호 개수 + 1
+	public int getStartPageOfPageGroup() {
+		return (getNowPageGroupNo() - 1) * pageGroupOfCount + 1;
+	}
+	
+	//현재 페이지 그룹 마지막 페이지 번호 : 현재 페이지 그룹번호 * 게시판 하단에 나타낼 페이지 번호 개수
+	//단 전체 페이지 개수가 작으면 전체 페이지 개수가 마지막 페이지 번호
+	public int getEndPageOfPageGroup() {
+		if(getNowPageGroupNo() * pageGroupOfCount > getTotalPage())
+			return getTotalPage();
+		return getNowPageGroupNo() * pageGroupOfCount;
+	}
+	
+	//이전 페이지 그룹이 있냐?
+	public boolean isPriviousPageGroup() {
+		return getNowPageGroupNo() > 1;
+	}
+	
+	//다음 페이지 그룹이 있냐?
+	public boolean isNextPageGroup() {
+		return getNowPageGroupNo() < getTotalPageGroup();
+	}
+
+	@Override
+	public String toString() {
+		return "PaggingVO [count=" + count + ", currentPageNo=" + currentPageNo + ", pageOfContentCount="
+				+ pageOfContentCount + ", pageGroupOfCount=" + pageGroupOfCount + "]";
+	}
+	
 	
 }
 
